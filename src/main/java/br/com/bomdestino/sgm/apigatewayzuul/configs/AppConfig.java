ad88d31fcec74ebd730364c8f51c13765a53d39b
João Pedro Amaral Souza <joaopedroa.6@hotmail.com>
2021-04-05 18:16:07 -0300

    Api Gateway Zuul

---
+++ src/main/java/br/com/bomdestino/sgm/apigatewayzuul/configs/AppConfig.java
@@ -0,0 +1,29 @@
+package br.com.bomdestino.sgm.apigatewayzuul.configs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@RefreshScope
+@Configuration
+public class AppConfig {
+
+	@Value("${jwt.secret}")
+	private String jwtSecret;
+	
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+		tokenConverter.setSigningKey(jwtSecret);
+		return tokenConverter;
+	}
+	
+	// Responsável por ler as informações do token
+	@Bean
+	public JwtTokenStore tokenStore() {
+		return new JwtTokenStore(accessTokenConverter());
+	}
+}
